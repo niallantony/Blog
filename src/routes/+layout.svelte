70229a72378fb54f575a4d5dc0a8e529db90b3f4
Niall Antony <88824327+niallantony@users.noreply.github.com>
2025-03-02 22:05:41 +0900

    Basic styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,32 @@
   let { children } = $props();
 </script>
 
-<nav>
-  <a href="/">Home</a>
-  <a href="/about">About</a>
-  <a href="/portfolio">Portfolio</a>
-  <a href="/blog">Blog</a>
-</nav>
-{@render children()}
+<div class="body">
+  <nav>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/portfolio">Portfolio</a>
+    <a href="/blog">Blog</a>
+  </nav>
+  <div class="content">
+    {@render children()}
+  </div>
+  <footer><a href="https://www.github.com/niallantony">github</a></footer>
+</div>
+
+<style>
+  .body {
+    display: flex;
+    flex-direction: column;
+    height: 100svh;
+  }
+  .content {
+    flex: 1;
+  }
+  nav {
+    justify-self: start;
+  }
+  footer {
+    justify-self: end;
+  }
+</style>
