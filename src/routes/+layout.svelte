9f125d69f5cdf1dffc2e409503ee7cda4ee3d5dc
Niall Antony <88824327+niallantony@users.noreply.github.com>
2025-03-03 14:54:10 +0900

    Add styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,10 +4,10 @@
 
 <div class="body">
   <nav>
-    <a href="/">Home</a>
-    <a href="/about">About</a>
-    <a href="/portfolio">Portfolio</a>
-    <a href="/blog">Blog</a>
+    <a href="/">home</a>
+    <a href="/about">about</a>
+    <a href="/portfolio">portfolio</a>
+    <a href="/blog">blog</a>
   </nav>
   <div class="content">
     {@render children()}
@@ -16,17 +16,62 @@
 </div>
 
 <style>
+  :root {
+    --light: #fffcf2;
+    --mid1: #ccc5b9;
+    --mid2: #403d39;
+    --accent: #eb5e28;
+    --dark: #252422;
+  }
   .body {
     display: flex;
     flex-direction: column;
     height: 100svh;
+    background-color: var(--dark);
   }
   .content {
     flex: 1;
+    overflow: scroll;
+    display: flex;
+    flex-direction: column;
   }
-  nav {
+  :global(h1, h2) {
+    font-family: "new-science", sans-serif;
+    font-weight: 300;
+    font-style: normal;
+    color: var(--light);
+  }
+  :global(p, a) {
+    color: var(--light);
+    font-family: "new-science", sans-serif;
+    font-weight: 400;
+    font-style: normal;
+  }
+  nav,
+  footer {
     justify-self: start;
+    display: flex;
+    justify-content: space-evenly;
+    background-color: var(--dark);
+    padding: 1rem 0;
+    margin: 0;
   }
+
+  nav {
+    border-bottom: 1px solid var(--accent);
+  }
+
+  nav > a,
+  footer > a {
+    color: var(--accent);
+    text-decoration: none;
+  }
+
+  nav > a:hover,
+  footer > a:hover {
+    filter: brightness(400%);
+  }
+
   footer {
     justify-self: end;
   }
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,60 @@
-<h1>About</h1>
+<h1>about</h1>
+<div class="about-layout">
+  <div class="block">
+    <h2>About Me</h2>
+    <p>
+      I have always had a passion for making things. Growing up, I found an
+      outlet for this in drawing and writing - stories, imaginary maps, board
+      games. I found a passion for engineering at school, and experimented
+      making robots, computer programs and building my own electric guitar. I
+      studied painting at university, and spent many years painting and
+      exhibiting. This was another tool I used as a creative outlet. Now I have
+      come back to computers, and spend my time learning and building, driven by
+      the same curiosity and motivation to create that first led me to pick up a
+      pencil.
+    </p>
+  </div>
+  <div class="block">
+    <h2>What I Believe</h2>
+    <p>
+      Technology is at its best when it brings people together, enhances
+      creativity, and makes knowledge more accessible. I believe in a future
+      where software is built responsibly, AI serves as a tool for inclusion,
+      and innovation is driven by curiosity and craft — not hype. As a
+      developer, I want to contribute to this vision by crafting systems that
+      are effective, ethical, and open to all.
+    </p>
+  </div>
+</div>
+
+<style>
+  h1 {
+    font-size: max(3rem, 6vw);
+    margin: 2rem 4rem 0;
+  }
+  h1::before,
+  h2::before {
+    content: "∎ ";
+    color: var(--accent);
+  }
+  .about-layout {
+    display: grid;
+    grid-template-columns: 1fr;
+    max-width: min(100%, 720px);
+    align-self: center;
+  }
+  .block {
+    margin: 1rem;
+  }
+  .block > h2 {
+    margin: 0 1rem;
+    text-transform: lowercase;
+  }
+  .block > p {
+    text-align: justify;
+    padding-left: 2rem;
+    margin-left: 2rem;
+    text-indent: -1rem;
+    border-left: solid 1px var(--accent);
+  }
+</style>
