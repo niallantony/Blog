faa5e369f2f5910759b0fae70fc5947a80d0ea25
Niall Antony <88824327+niallantony@users.noreply.github.com>
2025-03-08 10:43:03 +0900

    Remove unused database functions

--- src/lib/server/database.js
+++ src/lib/server/database.js
@@ -4,32 +4,6 @@ import { env } from "$env/dynamic/private";
 const uri = env.DBSTRING;
 const client = new MongoClient(uri);
 
-export async function findAllPosts() {
-  try {
-    await client.connect();
-
-    const database = client.db("myBlogDB");
-
-    const collection = database.collection("posts");
-
-    const cursor = await collection.find().toArray();
-
-    const posts = cursor.map((post) => {
-      return {
-        title: post.title,
-        tags: post.tags,
-        body: post.body,
-        added: post.added,
-      };
-    });
-    return posts;
-  } catch (err) {
-    console.error(err);
-  } finally {
-    await client.close();
-  }
-}
-
 export async function findPostTitles() {
   try {
     await client.connect();
@@ -105,40 +79,6 @@ export async function postPost(post) {
     return response;
   } catch (err) {
     console.log(err);
-  } finally {
-    await client.close();
-  }
-}
-
-export async function findDistinctDates() {
-  try {
-    await client.connect();
-
-    const database = client.db("myBlogDB");
-
-    const collection = database.collection("posts");
-
-    const cursor = await collection
-      .aggregate([
-        {
-          $group: {
-            _id: {
-              date: {
-                $dateToString: {
-                  format: "%Y-%m",
-                  date: "$added",
-                },
-              },
-            },
-          },
-        },
-      ])
-      .toArray();
-    const dates = cursor.map((date) => date._id.date);
-    console.log(dates);
-    return dates;
-  } catch (err) {
-    console.error(err);
   } finally {
     await client.close();
   }
