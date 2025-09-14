990ac86a7553f6d7110d63c7d7833e0749fd33f6
Niall Antony <88824327+niallantony@users.noreply.github.com>
2025-09-14 10:05:53 +0900

    Tweak styling of blog images to allow sizing

--- src/lib/BlogImage.svelte
+++ src/lib/BlogImage.svelte
@@ -1,20 +1,38 @@
 <script>
-  let { source, alt, children } = $props();
+  let { source, alt, children, size } = $props();
 </script>
 
 <figure>
-  <img src={source} {alt} />
+  <img src={source} {alt} class={size ? size : `medium`} />
   <figcaption>
     {@render children?.()}
   </figcaption>
 </figure>
 
 <style>
-  img {
-    max-width: 100%;
-  }
   figcaption {
     font-family: "new-science", sans-serif;
     text-align: center;
+  }
+  figure {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  img {
+    border-radius: 4px;
+  }
+
+  @media only screen and (min-width: 601px) {
+    .large {
+      max-width: 100%;
+    }
+    .medium {
+      max-width: 60%;
+    }
+    .small {
+      max-width: 40%;
+    }
   }
 </style>
